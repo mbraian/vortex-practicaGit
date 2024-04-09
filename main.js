e7de44d049cfd849436fb8a7dc699a1dbac182d0
LucasCorbalan <lucas.corbalan.23@gmail.com>
2024-04-09 10:23:43 -0300

    se agrego sintasix a main

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-const saludo = () => {
-    console.log("Hola wey!")
-};
+import React from 'react'
+
+const main = () => {
+  return (
+    <div>
+      Hola Braian
+    </div>
+  )
+}
+
+export default main
